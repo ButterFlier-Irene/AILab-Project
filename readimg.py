64fc5b78d8115f3a28b8705240b82fca6ed8a364
Julia Claudia <julia.claudiaist@gmail.com>
2024-05-22 19:37:07 +0200

    Final result
    Now we can have the coordinates of mediapipe along with the image matrix and the label (you need to access the second element of the tuple)

--- readimg.py
+++ readimg.py
@@ -1,9 +1,13 @@
 import os
+import cv2
+import pandas as pd
 import mediapipe as mp
 from torch.utils.data import Dataset
-from torchvision.io import read_image
-import pandas as pd
-import cv2
+
+landmarks = mp.solutions #solutions module of mediapipe contains the ML models for body parts
+hands_mp = landmarks.hands 
+hands = hands_mp.Hands(static_image_mode = True , min_detection_confidence = 0.3)
+
 class MpDataset(Dataset):
     #mandatory methods
     def __init__(self, labels_path, imgs_dir, transform = None):
@@ -11,6 +15,7 @@ class MpDataset(Dataset):
         self.imgs_dir= imgs_dir
         self.transform=transform #storing transform to apply
         self.labels = pd.read_csv(labels_path)
+        self.coordinates=[]
         #we are storing only labels, not images, since there could be a saturation problem
 
     def len(self):
@@ -22,13 +27,18 @@ class MpDataset(Dataset):
         img_path = os.path.join(self.imgs_dir, self.labels.iloc[index, 2], self.labels.iloc[index, 1])
         #now we have a directory and a set of filenames. We want to put them together
         img = cv2.imread(img_path)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  #we need to convert since otherwise mediapipe doesn't detect the landmarks.
         label = self.labels.iloc[index, 2]
-        if self.transform:
-            img =self.transform(img)
-        return img, label
+        if self.transform:                             #here we will apply mediapipe
+            result = self.transform(imgRGB)
+            if result.multi_hand_landmarks:
+                for hand_landmarks in result.multi_hand_landmarks:
+                    for dot in hand_landmarks.landmark:
+                            xy = [dot.x,dot.y]
+                            self.coordinates.append((xy))
+        return imgRGB, self.coordinates, label        #will return a tuple containing (image, the LIST of coordinates, label of the image)
 
 labels = pd.read_csv('dataset.csv')
-data = MpDataset('dataset.csv', 'Dataset_ASL')
-#img = data[6]
-#cv2.imshow('img', img[0])
-#cv2.waitKey(0)
+data = MpDataset('dataset.csv', 'Dataset_ASL', hands.process)
+img = data[100]
+print(img[1]) #it will print the coordinates of mediapipe
