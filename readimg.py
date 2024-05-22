b0bdeb117e6c46766bf5761e479b257f58bb00be
Julia Claudia <julia.claudiaist@gmail.com>
2024-05-22 15:10:43 +0200

    Created label file
    labels.csv: file containing the csv file needed to read onto the readimg.py file
    labels.py: program to create labels.csv file
    readimg.py: program containing the new csv file with all the images(still working on it)

--- readimg.py
+++ readimg.py
@@ -3,7 +3,7 @@ import mediapipe as mp
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 import pandas as pd
-'''
+
 class MpDataset(Dataset):
     #mandatory methods
     def __init__(self, labels_path, imgs_dir, transform = None):
@@ -26,46 +26,7 @@ class MpDataset(Dataset):
         if self.transform:
             img =self.transform(img)
         return img, label
-'''
-#dictionary of labels
-labels = {
-    0:'0',
-    1:'1',
-    2:'2',
-    3:'3',
-    4:'4',
-    5:'5',
-    6:'6',
-    7:'7',
-    8:'8',
-    9:'9',
-    10:'a',
-    11:'b',
-    12:'c',
-    13:'d',
-    14:'e',
-    15:'f',
-    16:'g',
-    17:'h',
-    18:'i',
-    19:'j',
-    20:'k',
-    21:'l',
-    22:'m',
-    23:'n',
-    24:'o',
-    25:'p',
-    26:'q',
-    27:'r',
-    28:'s',
-    29:'t',
-    30:'u',
-    31:'v',
-    32:'w',
-    33:'x',
-    34:'y',
-    35:'z'
-}
-#labels = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','t','u','v','x','y','z']
-#mp_dataset = MpDataset(labels, 'C:Dataset_ASL')
-labels = pd.read_csv(labels)
+
+#labels = pd.read_csv('labels.csv')
+data = MpDataset('labels.csv', 'Dataset_ASL')
+#print(data)
