8f95047d4a4dd48dd9a089c400608558ea06d9ac
Julia Claudia <julia.claudiaist@gmail.com>
2024-05-28 17:16:52 +0200

    Dataset

--- Model.py
+++ Model.py
@@ -1,31 +1,41 @@
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from sklearn.preprocessing import LabelEncoder
+#from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split, StratifiedKFold, RandomizedSearchCV
+import pickle
+from sklearn.model_selection import StratifiedKFold, RandomizedSearchCV
 import ast 
 from sklearn.decomposition import PCA as RandomizedPCA 
 from sklearn.pipeline import make_pipeline
 import seaborn as sns
+# defining pca model
+pca = RandomizedPCA(n_components=42, whiten=True, random_state=42)
+svc = SVC(kernel='rbf', class_weight='balanced', C = 100, gamma = 0.01)
+model = make_pipeline(pca, svc)
 
+C_range = list(np.logspace(-2, 10, 13))
+gamma_range = list(np.logspace(-9, 3, 13))
+param_dist = dict(svc__C=C_range, svc__gamma=gamma_range)
+grid = RandomizedSearchCV(model, param_dist, cv=skf, scoring='accuracy', n_iter=10)
+#####################################################################################
 data = pd.read_csv('coordinates.csv') #we have directly the file as a pandas dataframe
+print(len(data.labels))
+coo = np.array([np.array(co) for co in data.coordinates.apply(ast.literal_eval)]).reshape(2512, 42)
+target = np.array(data.labels)
 
 
 skf = StratifiedKFold(n_splits=5, shuffle=True)
-# Cross-validation
-for fold, (train_index, test_index) in enumerate(skf.split(data.coordinates,data.labels)):
-    X_train, X_test = data.coordinates[train_index], data.coordinates[test_index]
-    y_train, y_test = np.array(data.labels[train_index]), np.array(data.labels[test_index])
-
-X_train = np.array([np.array(co) for co in X_train.apply(ast.literal_eval)]).reshape(2010, 42)
-X_test = np.array([np.array(co) for co in X_test.apply(ast.literal_eval)]).reshape(502, 42) #(no of total values, no of rows for each , no of columns) 
-# print(X_train.shape)
-# print(X_test.shape)
-
-
-pca = RandomizedPCA(n_components=42, whiten=True, random_state=42)
+accuracy_scores = []
+######################################################################################
+for fold, (train_index, test_index) in enumerate(skf.split(coo, target)):
+    X_train_fold, X_test_fold = data.coordinates[train_index], data.coordinates[test_index]
+    y_train_fold, y_test_fold = data.labels[train_index], data.labels[test_index]
+    grid.fit(X_train_fold, y_train_fold)
+    #we need the parameters of the best score from cv_results_
+######################################################################################
+'''
 svc = SVC(kernel='rbf', class_weight='balanced', C = 100, gamma = 0.01)
 model = make_pipeline(pca, svc)
 model.fit(X_train, y_train)
@@ -36,23 +46,11 @@ y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 print('Accuracy:', acc) # 95% accuracy
 
-'''
-# Tuning the hyperparameters
-
-C_range = list(np.logspace(-2, 10, 13))
-gamma_range = list(np.logspace(-9, 3, 13))
-param_dist = dict(svc__C=C_range, svc__gamma=gamma_range)
-grid = RandomizedSearchCV(model, param_dist, cv=skf, scoring='accuracy', n_iter=10)
-grid.fit(X_train, y_train)
-
-# pd.DataFrame(grid.cv_results_).to_csv('grid_search_results.csv')
-
+pd.DataFrame(grid.cv_results_).to_csv('grid_search_results.csv')
+svc = SVC(kernel='rbf', class_weight='balanced', C = 100, gamma = 0.01)
+model = make_pipeline(pca, svc)
 print(grid.best_score_, grid.best_params_)
 print(grid.best_estimator_)
-'''
-
-
-'''
 # WIP
 
 fig, ax = plt.subplots(4, 6)
@@ -63,7 +61,6 @@ for i, axi in enumerate(ax.flat):
                         color='black' if y_pred[i] == y_test[i] else 'red') 
                 
 fig.suptitle('Predicted Names; Incorrect Labels in Red', size=14);
-'''
 
 
 # Clasification report
@@ -81,3 +78,6 @@ sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False,
 plt.xlabel('true label')
 plt.ylabel('predicted label');
 plt.show()
+'''
+
+
