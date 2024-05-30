aec42a63a456adee5c42f5626caea15f0f52bfef
Ionela Tataru <ionela.mda@gmail.com>
2024-05-30 13:03:57 +0200

    - Init `DrawBoundingBox.py` on the webcam with its prediction

--- detectSignsCamera.py
+++ detectSignsCamera.py
@@ -1,18 +1,20 @@
 import cv2
 import numpy as np
 import joblib
-from handLandmarks import GetLandmarks, DrawLandmarks
+from handLandmarks import GetLandmarks, DrawLandmarks, DrawBoundingBox
 
 labels = {'0':'0','1': '1', '2': '2', '3':'3','4':'4','5':'5','6':'6','7':'7','8':'8','9':'9','a':'A','b':'B','c':'C','d':'D','e':'E','f':'F','g':'G','h':'H','i':'I','j':'J','k':'K','l':'L','m':'M','n':'N','o':'O','p':'P','q':'Q','r':'R','s':'S','t':'T','u':'U','v':'V','w':'W','x':'X','y':'Y','z':'Z'}
-#VideoCapture
 
+# Load the pre-trained model
 OurModel = joblib.load("model.joblib")
 
+# Init videocapture
 cap = cv2.VideoCapture(0)
+
 
 while cap.isOpened():
     ret, img = cap.read()
-    image = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) #converting the channels
+    image = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) # converting the channels
     coordinates, result = GetLandmarks(image)
 
     if coordinates != '':
@@ -20,7 +22,9 @@ while cap.isOpened():
         coordinates = np.array(coordinates).reshape(1, 42)
         prediction = OurModel.predict(coordinates)
         predicted_character = labels[(prediction[0])]
+        img = DrawBoundingBox(img, result, predicted_character)
         print(predicted_character)
+        
     
     cv2.imshow('Detect Sign',img)
     k = cv2.waitKey(10)
